559e30a9c1598bd61a2d85b2e9b4ae6e6a7ae4ed
BWaldenmaier <BWaldenmaier@gmx.net>
2020-01-20 19:50:26 +0100

    Pause menu and scenes connected

---
+++ BackendTest/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+
+    public GameObject PauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {	
+		if (Input.GetKeyDown(KeyCode.Escape)){
+
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {              
+                Pause();
+            }
+
+        }    
+    }
+
+    public void Resume()
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1;
+        GameIsPaused = false;
+
+    }
+
+    public void Pause()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0;
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1;
+        Debug.Log("Loading Menu");
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting Game...");
+        Application.Quit();
+    }
+}
