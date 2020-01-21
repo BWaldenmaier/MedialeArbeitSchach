bfe7896bb3e070c1494235918aa102223e9526b8
BWaldenmaier <BWaldenmaier@gmx.net>
2020-01-21 01:34:17 +0100

    audiomanager + vorbereitung sound clips

--- BackendTest/Assets/Scripts/Board.cs
+++ BackendTest/Assets/Scripts/Board.cs
@@ -10,7 +10,8 @@ public class Board : MonoBehaviour
     public GameObject AddPiece(GameObject piece, int col, int row)
     {
         Vector2Int gridPoint = new Vector2Int(col, row);
-        GameObject newPiece = Instantiate(piece, new Vector3(gridPoint.x, -0.5f, gridPoint.y), Quaternion.identity);
+        GameObject newPiece = Instantiate(piece, new Vector3(gridPoint.x, -0.5f, gridPoint.y), Quaternion.identity);   
+          
         return newPiece;
     }
 
@@ -21,10 +22,89 @@ public class Board : MonoBehaviour
 
     public void SelectPiece(GameObject piece)
     {
+        Debug.Log(piece.name + "selected");
+        if (piece.name.Substring(0, piece.name.Length-11).Equals("Pawn_Pikachu")){
+            FindObjectOfType<AudioManager>().Play("Pikachu");
+        }
+        else if (piece.name.Substring(0, piece.name.Length-11).Equals("Pawn_Squirtle")){
+
+        }
+        else if (piece.name.Substring(0, piece.name.Length-11).Equals("Pawn_Bulbasaur")){
+
+        }
+        else if (piece.name.Substring(0, piece.name.Length-11).Equals("Pawn_Charmander")){
+
+        }
+        else if (piece.name.Substring(0, piece.name.Length-11).Equals("Pawn_Eevee")){
+
+        }
+        else if (piece.name.Substring(0, piece.name.Length-11).Equals("Pawn_Jigglypuff")){
+
+        }
+        else if (piece.name.Substring(0, piece.name.Length-11).Equals("Pawn_Psyduck")){
+
+        }
+        else if (piece.name.Substring(0, piece.name.Length-11).Equals("Pawn_Slowpoke")){
+            //not found
+        }
+        else if (piece.name.Substring(0, piece.name.Length-11).Equals("Bioshop_Arcanine")){
+            //not found
+        }
+        else if (piece.name.Substring(0, piece.name.Length-11).Equals("Bioshop_Growlithe")){
+
+        }
+        else if (piece.name.Substring(0, piece.name.Length-11).Equals("Bioshop_Ninetales")){
+
+        }
+        else if (piece.name.Substring(0, piece.name.Length-11).Equals("Bioshop_Vulpix")){
+
+        }
+        else if (piece.name.Substring(0, piece.name.Length-11).Equals("King_Mewtwo")){
+
+        }
+        else if (piece.name.Substring(0, piece.name.Length-11).Equals("King_Nidoking")){
+
+        }
+        else if (piece.name.Substring(0, piece.name.Length-11).Equals("Queen_Mew")){
+
+        }
+        else if (piece.name.Substring(0, piece.name.Length-11).Equals("Queen_Nidoqueen")){
+
+        }
+        else if (piece.name.Substring(0, piece.name.Length-11).Equals("Knight_Horsea")){
+
+        }
+        else if (piece.name.Substring(0, piece.name.Length-11).Equals("Knight_Seadra")){
+
+        }
+        else if (piece.name.Substring(0, piece.name.Length-11).Equals("Knight_Ponyta")){
+
+        }
+        else if (piece.name.Substring(0, piece.name.Length-11).Equals("Knight_Rapidash")){
+
+        }
+        else if (piece.name.Substring(0, piece.name.Length-11).Equals("Rook_Arbok")){
+
+        }
+        else if (piece.name.Substring(0, piece.name.Length-11).Equals("Rook_Ekans")){
+
+        }
+        else if (piece.name.Substring(0, piece.name.Length-11).Equals("Rook_Dragonair")){
+
+        }
+        else if (piece.name.Substring(0, piece.name.Length-11).Equals("Rook_Dratini")){
+
+        }
+        
+        
+        
+
         MeshRenderer renderers = piece.GetComponentInChildren<MeshRenderer>();
         renderers.material = selectedMaterial;
         Vector2Int gridPoint = GameManager.instance.GridForPiece(piece);
         piece.transform.position = new Vector3(gridPoint.x, 0, gridPoint.y);
+
+        
     }
 
     public void DeselectPiece(GameObject piece)
