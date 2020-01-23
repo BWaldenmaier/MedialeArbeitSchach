6aea0d67eb087fc274520573d29b9f0e508a4e55
BWaldenmaier <BWaldenmaier@gmx.net>
2020-01-23 20:17:31 +0100

    Audio not destroyed on start

---
+++ BackendTest/Assets/Scripts/DontDestroy.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+
+
+public class DontDestroy : MonoBehaviour{
+
+    void Awake()
+    {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("music");
+        if (objs.Length > 1)
+            Destroy(this.gameObject);
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
